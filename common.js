9fe6cc81761556eb1ac20042e6b65ef1e56dbabb
ryu08114 <87298247+ryu08114@users.noreply.github.com>
2021-07-19 12:31:44 +0900

    Update common.js

--- common.js
+++ common.js
@@ -1,17 +1,8 @@
 $(function() {
-  //外部ページから遷移
   var hash = location.hash,
       headerHeight = $('header').outerHeight();
-  if(hash) {
-    $('body,html').stop().scrollTop(0);
-    var target = $(hash),
-        targetPos = target.offset().top - headerHeight;
-    $('body,html').animate({scrollTop:targetPos},400);
-    return false;
-  }
-  
+      
   //ページ内遷移
-  
   $('a[href^="#"]').on('click',function() {
     var href = $(this).attr('href'),
         target = $(href),
@@ -19,4 +10,12 @@ $(function() {
     $('body,html').stop().animate({scrollTop:targetPos},400);
     return false;
   })
+  //外部ページから遷移
+  if(hash) {
+    $('body,html').stop().scrollTop(0);
+    var target = $(hash),
+        targetPos = target.offset().top - headerHeight;
+    $('body,html').animate({scrollTop:targetPos},400);
+    return false;
+  }
 })
