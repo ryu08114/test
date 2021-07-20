89fb48703a9bdde5d2c3cccfd14967653487388d
ryu08114 <87298247+ryu08114@users.noreply.github.com>
2021-07-20 14:57:20 +0900

    Update common.js

--- common.js
+++ common.js
@@ -1,27 +1,28 @@
 $(function() {
   var hash = location.hash,
-      headerHeight = $('header').outerHeight();
+      headerHeight = 0;
       
   //ページ内遷移
   $('a[href^="#"]').on('click',function() {
+    //headerがfixedの場合、headerの高さを取得
+    if($('header').css('position') == 'fixed') {
+      headerHeight = $('header').outerHeight();
+    }
     var href = $(this).attr('href'),
-        target = $(href),
-        targetPos = target.offset().top - headerHeight;
-    // $('body,html').stop().animate({scrollTop:targetPos},400);
-    scroll(targetPos);
-    return false;
+        target = $(href);
+    scroll(target);
   })
-  //外部ページから遷移
+  //外部ページから遷移（ハッシュがある場合）
   if(hash) {
-    $('body,html').stop().scrollTop(0);
-    var target = $(hash),
-        targetPos = target.offset().top - headerHeight;
+    $('body,html').stop().srcrollTop(0);
+    var target = $(hash);
     setTimeout(function() {
-      // $('body,html').animate({scrollTop:targetPos},400);
-      scroll(targetPos);
-    },300)
+      scroll(target);
+    },400)
   }
-  function scroll(targetPos) {
-    $('body,html').animate({scrollTop:targetPos},400);
+  //スクロールする処理を関数化
+  function scroll(target) {
+    var targetPos = target.offset().top - headerHeight;
+    $('body,html').stop().animate({scrollTop:targetPos},400);
   }
 })
