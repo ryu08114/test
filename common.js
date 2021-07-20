85e06252000325d889019e60a035ada2cbf4d060
ryu08114 <87298247+ryu08114@users.noreply.github.com>
2021-07-20 15:01:28 +0900

    Update common.js

--- common.js
+++ common.js
@@ -1,20 +1,21 @@
 $(function() {
   var hash = location.hash,
       headerHeight = 0;
+  
+  //headerがfixedの場合、headerの高さを取得
+  if($('header').css('position') == 'fixed') {
+    headerHeight = $('header').outerHeight();
+  }
       
   //ページ内遷移
   $('a[href^="#"]').on('click',function() {
-    //headerがfixedの場合、headerの高さを取得
-    if($('header').css('position') == 'fixed') {
-      headerHeight = $('header').outerHeight();
-    }
     var href = $(this).attr('href'),
         target = $(href);
     scroll(target);
   })
   //外部ページから遷移（ハッシュがある場合）
   if(hash) {
-    $('body,html').stop().srcrollTop(0);
+    $('body,html').stop().scrollTop(0);
     var target = $(hash);
     setTimeout(function() {
       scroll(target);
